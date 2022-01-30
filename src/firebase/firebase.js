5f706a4ba435cdfbcefa8597dd3ba4b8cbf9e6ff
Aditya Dubey <dubeyaditya6232@gmail.com>
2022-01-30 11:22:21 +0530

    added .env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,14 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA7RK4Ax4bN3EVWhQOjzZcOa3iDCIvHsLA",
-  authDomain: "todo-c77fd.firebaseapp.com",
-  projectId: "todo-c77fd",
-  storageBucket: "todo-c77fd.appspot.com",
-  messagingSenderId: "1001231317780",
-  appId: "1:1001231317780:web:6321dcd5272291961d6001",
-  measurementId: "G-EJ60ZX9QSM"
-};
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+}
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
